af223759c2a6f07f0459d4919407f85e54b2940d
contato.gallodev@gmail.com <contato.gallodev@gmail.com>
2020-06-11 12:02:46 -0300

    refactor li to div clear console error

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,39 +2,53 @@ import React from 'react'
 import Checkbox from './checkbox';
 import LocationInput from './LocationInput';
 import SelectBox from './Seletbox';
+import ClearFilter from './ClearFilter';
+import SearchButton from './SearchButton';
 
 export default function Search() {
     return (
-        <div className="search-content left col-xs-12">
-            <ul>
-                <li><Checkbox attr={{name:"new",label:"Novos"}} /></li>
-                <li><Checkbox attr={{name:"used",label:"Usados"}} /></li>                
-                <li className="clearfix mb-20"/>
-                <li className="left col-xs-3">
+        <div className="search-content left col-xs-12">            
+                <div>
+                    <Checkbox attr={{name:"new",label:"Novos"}} />
+                </div>
+                <div>
+                    <Checkbox attr={{name:"used",label:"Usados"}} />
+                </div>                
+                <div className="clearfix mb-20"/>
+                <div className="left col-xs-3">
                     <LocationInput/>
-                </li>
-                <li className="left col-xs-2">
+                </div>
+                <div className="left col-xs-2">
                     <SelectBox name="radius" label="Raio"/>
-                </li>
-                <li className="left ml-20 col-xs-3">
+                </div>
+                <div className="left ml-20 col-xs-3">
                     <SelectBox name="mark" label="Marca" class="w-200"/>
-                </li>
-                <li className="left ml-20 col-xs-3">
+                </div>
+                <div className="left ml-20 col-xs-3">
                     <SelectBox name="model" label="Modelo" class="w-200"/>
-                </li>
-                <li className="clearfix mb-20"/>
-                <li className="left col-xs-3">
+                </div>
+                <div className="clearfix mb-20"/>
+                <div className="left col-xs-3">
                     <SelectBox name="model" label="Ano Desejado" class="w-200"/>
-                </li>
-                <li className="left ml-20 col-xs-3">
+                </div>
+                <div className="left ml-20 col-xs-3">
                     <SelectBox name="model" label="Faixa de preço" class="w-200"/>
-                </li>
-                <li className="left ml-20 col-xs-5">
+                </div>
+                <div className="left ml-20 col-xs-5">
                     <SelectBox name="model" label="Versão" class="w-200"/>
-                </li>
-                <li className="clearfix mb-20"/>
-
-            </ul>
+                </div>
+                <div className="clearfix mb-40"/>
+                <div className="left col-xs-6">
+                    <a href="#a"> > Busca avançada </a>
+                </div>
+                <div className="left col-xs-6">
+                    <div className="left col-xs-4">
+                        <ClearFilter/>
+                    </div>
+                    <div className="left col-xs-8">
+                        <SearchButton/>
+                    </div>
+                </div>                            
         </div>
     )
 }
